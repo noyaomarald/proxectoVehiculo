db92222247ad6fe97b2cf331fc00b9a92e684757
DAM1_Alu13 <DAM1_Alu13@equip077238.xunta.local>
2021-05-04 16:27:17 +0200

    Engadida mensaxe de saudo

---
+++ src/main/java/Proba1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DAM1_Alu13
+ */
+public class Proba1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Git!");
+    }
+}
